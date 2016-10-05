b7c33e932bb84b5789e13d7188c369cf8d823d3c
Emma-Louise Lynch <Emma-Louise.Lynch@paddypower.com>
2016-10-05 14:33:25 +0100

    Daily Programmer 2016-09-12
    Improved anagram detector - using scala standard methods instead of regex
    Added check to ensure re-ordered words weren't detected as anagrams

--- src/main/scala/DP_2016_09_12/AnagramDetector.scala
+++ src/main/scala/DP_2016_09_12/AnagramDetector.scala
@@ -3,10 +3,12 @@ package DP_2016_09_12
 object AnagramDetector {
 
   def isAnagram(input: String, anagram: String): Boolean = {
-    val strippedInput = input.replaceAll("[^a-zA-Z]", "")
-    val strippedAnagram = anagram.replaceAll("[^a-zA-Z]", "")
 
-    strippedInput.size == strippedAnagram.size &&
-      strippedInput.toLowerCase.sortWith(_ < _) == strippedAnagram.toLowerCase.sortWith(_ < _)
+    val inputLetters = input.filter(_.isLetter).toLowerCase
+    val anagramLetters = anagram.filter(_.isLetter).toLowerCase
+
+    inputLetters.size == anagramLetters.size &&
+      inputLetters.sorted == anagramLetters.sorted &&
+        input.toLowerCase.split(" ").sorted.deep != anagram.toLowerCase.split(" ").sorted.deep
   }
 }
--- src/test/scala/DP_2016_09_12/AnagramDetectorTest.scala
+++ src/test/scala/DP_2016_09_12/AnagramDetectorTest.scala
@@ -1,16 +1,18 @@
 package DP_2016_09_12
 
 import org.scalatest.{FlatSpec, Matchers}
+import AnagramDetector.isAnagram
 
 class AnagramDetectorTest extends FlatSpec with Matchers{
 
   it should "correctly detect anagrams" in {
-    AnagramDetector.isAnagram("wisdom", "mid sow") shouldBe true
-    AnagramDetector.isAnagram("Seth Rogan", "Gathers No") shouldBe true
-    AnagramDetector.isAnagram("Reddit", "Eat Dirt") shouldBe false
-    AnagramDetector.isAnagram("Schoolmaster", "The classroom") shouldBe true
-    AnagramDetector.isAnagram("Astronomers", "Moon starer") shouldBe false
-    AnagramDetector.isAnagram("Vacation Times", "I'm Not as Active") shouldBe true
-    AnagramDetector.isAnagram("Dormitory", "Dirty Rooms") shouldBe false
+    isAnagram("wisdom", "mid sow") shouldBe true
+    isAnagram("Seth Rogan", "Gathers No") shouldBe true
+    isAnagram("Reddit", "Eat Dirt") shouldBe false
+    isAnagram("Schoolmaster", "The classroom") shouldBe true
+    isAnagram("Astronomers", "Moon starer") shouldBe false
+    isAnagram("Vacation Times", "I'm Not as Active") shouldBe true
+    isAnagram("Dormitory", "Dirty Rooms") shouldBe false
+    isAnagram("Old Man", "Man Old") shouldBe false
   }
 }
